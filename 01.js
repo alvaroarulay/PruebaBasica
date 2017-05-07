7ff3e58163b229977657a555933b652307c7eb0e
Alvaro Arquuipa <alvarolp1@gmail.com>
2017-05-07 09:56:47 -0400

    nuevo doc

---
+++ 01.js
@@ -0,0 +1,4 @@
+var y=1,
+	z=2
+y=y+z++
+console.log(y)
---
+++ negacion.js
@@ -0,0 +1,12 @@
+/*console.log("!false:  "+(!false))
+console.log("!0:  "+(!0))
+console.log("!7:  "+(!7))
+console.log("!pepe:  "+(!"pepe"))
+console.log('!"":  '+(!""))
+*/
+var x=5,
+	y="5"
+console.log("x=  "+x)
+console.log("y=  "+'"'+y+'"')
+console.log("x==y:  "+(x==y))
+console.log("x===y:  "+(x===y))
